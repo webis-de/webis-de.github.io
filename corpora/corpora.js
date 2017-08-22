b585031e41a0ee9863217767302fcfff704a9f1f
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2017-08-22 09:47:46 +0200

    Add files via upload

---
+++ corpora/corpora.js
@@ -0,0 +1,10 @@
+/* DATE. June 20th, 2013 */
+$(document).ready(function() {
+$('.tablesorter').dataTable( {
+  'bPaginate': false,
+  'bLengthChange': false,
+  'bFilter': false,
+  'bInfo': false,
+  'bAutoWidth': false
+});
+});
