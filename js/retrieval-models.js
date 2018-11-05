21c83fa88e756ddfb36cbdec576006a4cc08b013
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2018-11-05 12:12:08 +0100

    retrieval models cleanup

--- js/retrieval-models.js
+++ js/retrieval-models.js
@@ -1,23 +1,19 @@
 /* DATE. October 30th, 2018 */
 
-function showPopup(fragment) {
-  const oldPopups = document.querySelectorAll('#popupContent .popup');
-  for (var i = 0; i < oldPopups.length; ++i) {
-    oldPopups[i].setAttribute('style','display:none;opacity:0');
+function showPopup(id) {
+  const lastPopup = document.querySelector('.popup-active');
+  if (lastPopup != null) {
+    lastPopup.classList.remove('popup-active');
   }
 
-  const newPopup = document.querySelector(fragment);
-  newPopup.setAttribute('style','display:block;opacity:0');
-  setTimeout(function() {
-    newPopup.setAttribute('style','display:block;opacity:1');
-  }, 100);
+  const newPopup = document.querySelector(id);
+  newPopup.classList.add('popup-active');
 }
 
 const map = document.querySelector('#retrieval-models-map');
 map.addEventListener('click', function(event) {
   event.preventDefault();
-  const id = event.target.getAttribute('href');
-  showPopup(id);
+  showPopup(event.target.getAttribute('href'));
 });
 
-showPopup("#boolean-box");
+showPopup('#boolean-box');
