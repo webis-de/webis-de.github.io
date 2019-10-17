e62a996503079a91f5ea99531630f09a70d625b8
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2019-10-17 12:22:04 +0200

    update

--- js/bibentry-webis-de.js
+++ js/bibentry-webis-de.js
@@ -10,7 +10,7 @@ doFilterWebisDe = (query) => {
         filteredAllMessage.setAttribute("aria-hidden", "true");
     }
 
-    document.location.hash = "#filter:" + query;
+    document.location.hash = "#?q=" + query;
 
     // Force UIkit update to prevent glitches
     UIkit.update();
@@ -18,11 +18,28 @@ doFilterWebisDe = (query) => {
 
 // Set up filter field
 const filterField = document.getElementById("bib-filter-field");
+// legacy 'filter:' option
 if (document.location.hash.startsWith("#filter:")) {
-    filterField.value = decodeURIComponent(document.location.hash.substr(8));
+    const query = decodeURIComponent(document.location.hash.substr(8));
+    document.location.hash = "#?q=" + query;
+}
+if (document.location.hash.startsWith("#?q=")) {
+    const query = decodeURIComponent(document.location.hash.substr(4));
+    filterField.value = query;
 }
 filterField.addEventListener("input", event => doFilterWebisDe(event.target.value));
 activateBibtexToggle(document);
 doFilterWebisDe(filterField.value);
 filterField.focus();
+
+// Update if hash in URL changed (e.g., browser back button)
+window.addEventListener("hashchange", event => {
+    if (document.location.hash.startsWith("#?q=")) {
+        const query = decodeURIComponent(document.location.hash.substr(4));
+        if (query !== filterField.value) {
+          filterField.value = query;
+          doFilterWebisDe(query);
+        }
+    }
+});
 
